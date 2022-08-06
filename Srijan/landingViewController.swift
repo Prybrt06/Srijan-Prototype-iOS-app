c6304ea4eefa13074b90132f4f51fd10fdef0d60
Prybrt06 <prybrt06@gmail.com>
2022-08-06 15:09:02 +0530

    Annimation added

--- Srijan/landingViewController.swift
+++ Srijan/landingViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class landingViewController: UIViewController
 {
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0
+        let titleText = "SRIJAN 3.0"
+
+        for letter in titleText
+        {
+            Timer.scheduledTimer(withTimeInterval: 0.08 * Double(charIndex), repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+
+            charIndex += 1
+        }
     }
 }
